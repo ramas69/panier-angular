62f863318798072d536805956514f93298ff0147
rama69 <ramatoulaye.sou@gmail.com>
2023-11-07 11:43:18 +0100

    panier fruit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,78 @@
+import { Component, OnInit } from '@angular/core';
+import { IProduit } from '../IProduit';
+import { ProduitService } from '../produit.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  produits:IProduit[]=[];
+  commandes:IProduit[]=[];
+  resultat :number = 0;
+
+  constructor(private produitS:ProduitService){}
+
+  ngOnInit(): void {
+      this.getProduit();
+  }
+
+
+// recuperer les produits
+  getProduit(){
+    this.produits= this.produitS.fetchAll()
+    return this.produits
+  }
+
+  //ajout article dans le panier avec la quantité
+  ajouterPanier(index: number) {
+   this.produitS.addPanier(index);
+    this.commandes = this.produitS.commande
+    console.log(this.commandes);
+    
+  }
+
+
+  //incrementation quantité  produit
+  addQuantity(index:number){
+    this.produits[index].quantite++;
+  }
+
+  // decrementation quantité produit
+  deleteQuantity(index:number){
+    this.produits[index].quantite--;
+  // pour ne pas desecndre en dessous de 0
+    if(this.produits[index].quantite<0){
+        this.produits[index].quantite=0;
+      }
+  
+   
+  }
+
+
+  deletePoduit(index:number){
+    this.produitS.deletePanier(index);
+    this.commandes = this.produitS.commande
+  }
+
+
+  Total(index:number){
+    return this.commandes[index].quantite*this.commandes[index].prixHT;
+
+  }
+
+  calculerTotal(): number {
+    return this.commandes.reduce((acc, commande) => acc + Number(commande.prixHT) * commande.quantite, 0);
+  }
+  
+  calculerQte(): number {
+    return this.commandes.reduce((acc, commande) => acc + commande.quantite, 0);
+  }
+
+  supprimePanier(){
+    this.commandes = [];
+  }
+
+
+}
